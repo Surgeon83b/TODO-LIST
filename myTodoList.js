e0c6bab5bda1de9c3862b214cdacd5af76fed535
Kanstantsin Piatkevich <surgeon-2@tut.by>
2021-12-20 23:56:04 +0300

    myTodoList_v2

--- myTodoList.js
+++ myTodoList.js
@@ -1,29 +1,40 @@
-/*function save() {
-    localStorage.setItem('item', document.querySelector('.td_items').innerHTML);
-  }
-
-function init() {
-    const fromStorage = localStorage.getItem('item');
-    if (fromStorage) {
-      document.querySelector('.td_items').innerHTML = fromStorage;
-    }
-   // document.querySelector('.todo__options').addEventListener('change', this.update);
-}*/
-
-function addItem() {
-    let tdI = document.getElementById('todo_item');
-    if (tdI.value) {
-        document.getElementById('td_items').innerHTML += '<li>'+tdI.value+'</li>';
-       // save();
-    }
-        tdI.value = '';
-        tdI.focus();
+// saving todo_item into localStorage
+function save(elem) {
+    localStorage.setItem(++localStorage.length, elem);
 }
 
+// restoring data from localStorage
+function init() {
+    let it = "";
+    if (localStorage.length)
+        for (let i = 1; i <= localStorage.length; i++) {
+            it = localStorage.getItem(i);
+            document.getElementById('td_items').innerHTML += '<li>'+ it +'</li>';
+    }
+}
+
+// adding todo_item to the HTML-list and to localStorage
+function addItem() {
+    tdI = document.getElementById('todo_item');
+    if (tdI.value) {
+        document.getElementById('td_items').innerHTML += '<li>'+ tdI.value +'</li>';
+        save(tdI.value);
+        tdI.value = '';
+    }   
+    tdI.focus();
+}
+
+// reaction on Enter pressing
 function keyPress(event) {
     if (event.which == 13) addItem();
 }
 
-/*window.onload = function() {
+// reaction on F5 pressing
+window.onload = function() {
     init();
-}*/
+}
+
+// clear localStorage on page loading
+document.onload = function() {
+    localStorage.clear();
+}
